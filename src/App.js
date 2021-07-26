9d65c14ced395cdc9562152648ecacb6f5ab8ab6
Morgane Deschamps <morgane.deschamps@gmail.com>
2021-07-26 13:55:19 +0200

    search authors is working - needs refininf

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
 import * as apiSearches from "./Services/api.services"
+import { useEffect, useState } from 'react';
+
+//searchAny needs  {searchType: , subSearch: , searchTerms:}
+//searchType: "main" or "notMain"
+//subSearch: "searc" / "authors" / "subjects"
+//searchTerms:
+//- for main {q: "keywords"} / {title:, author:, subject:, place:, person:, language:, publisher:}
+//- for authors {q: "keywords"} 
+//- for subjects {q: "keywords"} /  {q: "keywords", subject_type: "place" / "time" / "person"}
+
 
 function App() {
 
 
-  function findStuff() {
-    const fnA = apiSearches.getAuthorList()
-    return fnA.map(eachDoc => apiSearches.accessAuthorWorks(eachDoc.key))
+  const [authorsState, setAuthors] = useState([])
+  const [worksState, setWorks] = useState([])
+  const [searchState, setSearch] = useState("")
+
+
+  function findAuthor(searchEntry) {
+    apiSearches.searchAny({
+      searchType: apiSearches.notMain, 
+      subSearch: "authors", 
+      searchTerms:{q: searchEntry}})
+    .then(res => setAuthors(res))
+    .catch(err => console.log(err))
+
+  }
+
+  //    console.log("fna: ", fnA)
+  //fnA.then(res => res.map(eachDoc => apiSearches.accessAuthorWorks(eachDoc.key))).catch(err => console.log(err))
+
+
+  function handleChange(event) {
+    setSearch(event.target.value)
   }
 
 
 
+  function handleSubmit(event) {
+    event.preventDefault()
+    findAuthor(searchState)
+  }
 
+  useEffect(() => console.log("authorsState: ", authorsState), [authorsState])
 
 
   return (
     <div className="App">
-      <button onClick={findStuff}>Search author</button>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="author">Search for authors: </label>
+        <input type="text" onChange={handleChange} value={searchState} />
+        <button type="submit">Search</button>
+      </form>
+
+      {(authorsState.length === 0) && <div>Loading...</div>}
+
+      {authorsState.map((eachAuthor) => {
+        return <h4>{eachAuthor.name}</h4>
+      })}
+
     </div>
   );
 }
--- src/Services/api.services.js
+++ src/Services/api.services.js
@@ -1,4 +1,58 @@
 import axios from 'axios';
+let URI = require('urijs');
+let URITemplate = require('urijs/src/URITemplate');
+
+
+
+//CREATE URLS FOR SEARCHES
+
+//new URITemplate(`https://app.ticketmaster.com/discovery/v2/{resource}.json{?q*,apikey}`);
+/*let searchUri = uriTemplate.expand({
+    resource: "events",
+    q: {
+        size: "50", 
+        keyword, 
+        city: citySearched,
+        localStartDateTime: dateApi
+    },
+    apikey: apiKey
+  })
+*/
+
+/*
+MainSearch URL = http://openlibrary.org/search.json?{query1}={queryValue1}&{query2}={queryValue2}
+=> keyword search : q=
+=> specific queries: title, author, subject, place, person, language, publisher
+
+SubjectSearch URL = https://openlibrary.org/search/subjects.json?q={queryValue}+subject_type:{queryValue}
+=> subject_type options: place, time, person
+
+AuthorSearch URL = https://openlibrary.org/search/authors.json?q={queryValue1}
+
+
+
+
+https://openlibrary.org/search.json?title=harry+potter+stone&language=eng
+https://openlibrary.org/search.json?title=Harry+Potter&language=en
+*/
+
+export const main = new URITemplate(`https://openlibrary.org/{subSearch}.json{?q*}`)
+export const notMain = new URITemplate(`https://openlibrary.org/search/{subSearch}.json{?q*}`)
+//works for authors and subjects
+
+
+
+
+function testUrl(searchOptions) {
+    const {searchType, subSearch, searchTerms} = searchOptions;
+
+    let searchUrl = searchType.expand({
+        subSearch: subSearch,
+        q: {...searchTerms}
+    })
+    console.log("url is:", searchUrl)
+    return searchUrl
+}
 
 
 const urlbase = axios.create({
@@ -6,15 +60,17 @@ const urlbase = axios.create({
 })
 
 
-export function getAuthorList() {
-    urlbase.get("/search/authors.json?q=rowling")
-    .then(res => console.log(res.data.docs))
+export function searchAny(searchProps) {
+    
+    return axios.get(testUrl(searchProps))
+    .then(res => {return res.data.docs})
     .catch(err => console.log(err))
     
 }
 
+
 export function accessAuthorWorks(key) {
-    const url = `/${key}/works.json`
+    const url = `/authors/${key}/works.json`
 
     urlbase.get(url)
     .then(res => console.log(res.data))
